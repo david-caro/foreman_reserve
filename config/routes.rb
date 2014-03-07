1cd9be5266b4d4cbb58c6add60894c0322d30a1e
Daniel Lobato <elobatocs@gmail.com>
2014-03-07 13:11:11 +0100

    Adapted code for API v2 standards and 1.5
    Style changes
    
    Removed Gemfile
    
    Redefined old routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,27 @@
 Rails.application.routes.draw do
 
-  get "api/hosts_reserve"          => "reserve#reserve"
-  get "api/hosts_release"          => "reserve#release"
-  get "api/show_available"         => "reserve#show_available"
-  get "api/show_reserved"          => "reserve#show_reserved"
-  get "api/update_reserved_reason" => "reserve#update_reason"
+  namespace :api, :defaults => {:format => 'json'} do
+    get 'hosts_reserve'          => 'v2/reserves#reserve'
+    get 'hosts_release'          => 'v2/reserves#release'
+    get 'show_available'         => 'v2/reserves#show_available'
+    get 'show_reserved'          => 'v2/reserves#show_reserved'
+    get 'update_reserved_reason' => 'v2/reserves#update_reason'
+
+    scope "(:apiv)", :module => :v2,
+                     :defaults => {:apiv => 'v2'},
+                     :apiv => /v1|v2/,
+                     :constraints => ApiConstraints.new(:version => 2) do
+
+      resources :reserves, :only => [] do
+        collection do
+          get 'reserve'
+          get 'release'
+          get 'show_available'
+          get 'show_reserved'
+          get 'update_reserved_reason' => 'reserves#update_reason'
+        end
+      end
+    end
+  end
 
 end
